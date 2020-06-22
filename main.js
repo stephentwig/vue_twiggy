b6f99e1116f94c93055c0a28c72f5993d36af81a
stephentwig <step.twig@gmail.com>
2020-06-22 21:52:31 +0100

    added url and image for attribure binding v-bind

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var app = new Vue ({
     el: '#app',
     data: {
-        product: 'Socks'
+        product: 'Socks',
+        image: 'images/vmSocks-green-onWhite.jpg',
+        url: 'https://vue.js'
 
     }
 })
